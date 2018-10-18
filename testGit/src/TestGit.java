b3d5010f33b8144058cd2d8d01bd450c38c6a23e
fqzEmily <qiaozhenjk112@163.com>
2018-10-18 16:02:57 +0800

    testGit

---
+++ testGit/src/TestGit.java
@@ -0,0 +1,18 @@
+/**
+ * Created by fuqiaozhen   on 2018/10/18
+ */
+public class TestGit {
+    public static void main(String[] args) {
+
+        System.out.println(test1());
+        System.out.println(test2());
+    }
+
+    private static String test1(){
+        return "hello test1";
+    }
+
+    private static String test2(){
+        return "hello test2";
+    }
+}
